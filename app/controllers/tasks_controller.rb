7e0b13606107b4af28d02e53f806b4432c9fa92b
Ingrid Gustave <ingrid.gustave@gmail.com>
2024-01-29 21:24:29 +0100

    Finished Rails task manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -32,9 +32,10 @@ class TasksController < ApplicationController
   end
 
   private
-def set_task
-  @task = Task.find(params[:id])
-end
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
